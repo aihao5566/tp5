d01eec7c9e07e71ff181f4cd3d03bbf992d26752
zws <619216299@qq.com>
2019-02-05 20:17:29 +0800

    修改了理解

--- application/index/behavior/Test.php
+++ application/index/behavior/Test.php
@@ -6,10 +6,9 @@ namespace app\index\behavior;
 use think\Hook;
 
 class Test {
-    //只有run()方法的时候run会自动执行(存在对应的行为名时这里不执行,自定义的除外)
     //前提是定义过行为路径
     public function run(&$params){  //定义了行为类就执行(tags)
-//        echo '<script>alert("我是run");</script>';
+        echo '<script>alert("我是run");</script>';
 //        echo '钩子(行为)init'.'<br />';//因为系统本身定义了监听位，所以只要定义了这里就执行,控制器或前端在定义一次的话就输出两次
         //在behavior定义的类,直接运行run方法,在把这个test1加入,直到监听listen才运行
         Hook::add('test1','app\\demo\\behavior\\Demo');//添加行为
@@ -21,20 +20,9 @@ class Test {
 //        echo '<script>alert("我是行为moduleInit");</script>';
 //    }
 
-    //应用开始
-//    public function appInit(&$params)
-//    {
-//        echo '<script>alert("我是行为appInit");</script>';
-//    }
-
-//    public function appEnd(&$params)
-//    {
-//        echo '<script>alert("我是行为appEnd");</script>';
-//    }
-//
-    //只执行自定义的行为run留空，不能没有run方法
-//    public function test(&$params)
-//    {
-//        echo '<script>alert("'.$params['a'].'");</script>';
-//    }
+    //只执行自定义的行为
+    public function test2(&$params)
+    {
+        echo '<script>alert("'.$params['a'].'");</script>';
+    }
 }
--- application/index/tags.php
+++ application/index/tags.php
@@ -2,13 +2,11 @@
 // 当前模块行为扩展定义文件
 return [
     'module_init'=> [
-        'app\\index\\behavior\\Test', //注意行为的命名空间(下同)
+//        'app\\index\\behavior\\Test', //如果定义了该类,没有实现该方法，会执行run()方法
     ],
-    'test'=> [//自定义标签位
-        'app\\demo\\behavior\\Demo',
-    ],
-    'module_end'=> [
+
+    'test2'=> [//自定义标签位,如果该类没有这个方法写上run()方法,不会报错
         'app\\index\\behavior\\Test',
     ],
-    //................
+
 ];
